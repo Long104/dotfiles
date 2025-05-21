3426f1fc003fd8a0c91bd7d6506ef28c7015d07a
pantorn <longpantorn@gmail.com>
2025-05-21 22:39:46 +0700

    update nix package and nvim config

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -5,8 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     darwin = {
       url = "github:lnl7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+      inputs.nixpkgs.follows = "nixpkgs"; };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
